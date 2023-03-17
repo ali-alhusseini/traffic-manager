2f1f7fe667d14b3c0f71aac881bbcd0010625b59
Ali Alhusseini <ciwanbuhar@gmail.com>
2023-03-17 14:44:22 -0300

    Open sequence.txt file

--- manager.c
+++ manager.c
@@ -7,12 +7,24 @@
 
 int main(int argc, char *argv[])
 {
+    // Check for command line input
     if (argc == 2)
     {
          double p = argv[1];
     }
     else {
         printf("Wrong number of command line arguments.\n");
+    }
+
+    // Open seuqnce.txt file
+    FILE* file;
+    char ch;
+
+    file = fopen("sequence.txt", "r");
+
+    if (NULL == file)
+    {
+        printf("sequence.txt couldn't be opened.");
     }
 
 }
