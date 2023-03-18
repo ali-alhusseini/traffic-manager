9a6e520e84debb57700da88cc8a6625f77b76a07
R0keefe <rileyjsix@gmail.com>
2023-03-18 17:42:35 -0300

    Add libraries to manager.c

--- manager.c
+++ manager.c
@@ -1,8 +1,9 @@
-#include <sys/types.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <semaphore.h>
 
 
 int main(int argc, char *argv[])
